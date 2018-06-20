239aa1475fae622cf19c6402dca3b06d07ee237b
Ben Titcomb <btitcomb@scpr.org>
2018-06-19 18:21:51 -0700

    Remove generators and tests that we don't need to implement.

--- blueprints/initializer/files/__root__/initializers/__name__.js
+++
@@ -1,9 +0,0 @@
-import Application from '@ember/application';
-
-export function initialize(application) {
-  // application.inject('route', 'foo', 'service:foo');
-}
-
-export default {
-  initialize
-};
